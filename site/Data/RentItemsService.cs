12d154922aaaa9f4c4aa13b9b3ad02b088b994ef
Johannes Bertens <johannes.bertens@gmail.com>
2020-04-25 18:09:21 +0200

    Moved from Cosmos to SQL Azure

--- site/Data/RentItemsService.cs
+++ site/Data/RentItemsService.cs
@@ -1,39 +1,53 @@
-﻿using Azure.Cosmos;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text.Json;
+using System.Data;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Text;
 
 namespace site.Data
 {
     public class RentItemsService
     {
-        public static async Task<RentItem[]> GetRentItemsAsync()
+        public static async Task<RentItem[]> GetRentItemsAsync(ApplicationUser user)
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables() 
+                .AddEnvironmentVariables()
                 .Build();
 
-            var EndpointUri = config["APPSETTING_CosmosEndpointUri"];
-            var PrimaryKey = config["APPSETTING_CosmosPrimaryKey"];
-            
-            var databaseId = "rentstorage";
-            var containerId = "rentstorage";
+            using SqlConnection connection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            connection.Open();
 
-            var cosmosClient = new CosmosClient(EndpointUri, PrimaryKey,
-                new CosmosClientOptions() {ApplicationName = "CosmosDBDotnetQuickstart"});
-            var container = cosmosClient.GetContainer(databaseId, containerId);
-            Console.WriteLine($"Got Container: {container.Id}\n");
+            StringBuilder sb = new StringBuilder();
+            sb.Append("SELECT [city], [name], [slot], [item], [character] ");
+            sb.Append("FROM [dbo].[RENTSTORAGE] ");
+            sb.Append($"WHERE email = @email and personalkey = @key");
+            var sql = sb.ToString();
 
-            var queryDefinition = new QueryDefinition($"SELECT * FROM c");
+            using SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.Add("@email", SqlDbType.VarChar);
+            command.Parameters["@email"].Value = user.Email;
+            command.Parameters.Add("@key", SqlDbType.VarChar);
+            command.Parameters["@key"].Value = user.PersonalKey;
+
+
+            using SqlDataReader reader = command.ExecuteReader();
             var rentItemList = new List<RentItem>();
-            await foreach (JsonElement item in container.GetItemQueryIterator<JsonElement>(queryDefinition))
+            while (await reader.ReadAsync())
             {
-                rentItemList.Add(JsonConvert.DeserializeObject<RentItem>(item.GetRawText()));
+                var rentItem = new RentItem()
+                {
+                    city = reader.GetString(0),
+                    name = reader.GetString(1),
+                    slot = reader.GetInt32(2),
+                    item = reader.GetString(3),
+                    character = reader.GetString(4)
+                };
+                Console.WriteLine(rentItem);
+                rentItemList.Add(rentItem);
             }
 
             return rentItemList.ToArray();
--- site/Pages/Chars/Storage.razor.cs
+++ site/Pages/Chars/Storage.razor.cs
@@ -4,16 +4,20 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace site.Pages.Chars
 {
     public partial class Storage: IDisposable
     {
-        private RentItem[] rentItems;
+        public RentItem[] RentItems { get; set; }
 
-        [Inject]
-        public IJSRuntime JSRuntime { get; set; }
+        [Inject] public IJSRuntime JSRuntime { get; set; }
 
+        [Inject] public IHttpContextAccessor Context { get; set; }
+
+        [Inject] public UserManager<ApplicationUser> UserManager { get; set; }
 
         [CascadingParameter]
         private Task<AuthenticationState> authenticationStateTask { get; set; }
@@ -21,11 +25,11 @@ namespace site.Pages.Chars
         protected override async Task OnInitializedAsync()
         {
             var user = (await authenticationStateTask).User;
-
             if (user.Identity.IsAuthenticated)
             {
                 // Perform an action only available to authenticated (signed-in) users.
-                rentItems = await RentItemsService.GetRentItemsAsync();
+                var applicationUser = await UserManager.GetUserAsync(Context.HttpContext.User);
+                RentItems = await RentItemsService.GetRentItemsAsync(applicationUser);
             }
         }
 
